6c3a836ea344d27d1eb3ab492739b27d41f3038b
Evan Marshall <evanmarshall@Evans-MacBook-Air.local>
2017-09-27 12:17:04 -0300

    started carousel and mfb. Fix the prev button on carousel.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+$(document).ready(function(){
+	
+	$('.hero').slick({
+		dots: true,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		fade: true,
+		cssEase: 'linear'
+  	});
+	
+});
