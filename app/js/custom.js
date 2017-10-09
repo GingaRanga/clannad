6ce28f76765b82e26cb8a0a864917f6650dd612f
Evan Marshall <contact@elysianwebdesign.com>
2017-10-09 15:56:55 -0300

    setting up full page layout, figure out how to vertically center within carousel, then work on styling and animation.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 	$('.hero').slick({
-		dots: true,
+		dots: false,
 		slidesToShow: 1,
 		adaptiveHeight: true,
 		slidesToScroll: 1,
@@ -15,15 +15,33 @@ $(document).ready(function(){
 	//GSAP
 	var topNav 	= $('.header-top');
 	
-	// you can also use pure JavaScript instead of jQuery such as
-	// or var applyBar = document.getElementById('.apply');
+	/*
+		you can also use pure JavaScript instead of jQuery such as
+		or var applyBar = document.getElementById('.apply');
+		you can use fromto method as long as you set a starting and ending value or set method to just set a CSS property
+		set does not require a duration
+	*/
 	
-	// you can use fromto method as long as you set a starting and ending value or set method to just set a CSS property
-	// set does not require a duration
+	TweenLite.to(topNav, 3, {
+		autoAlpha: 0, 
+		y:-46, 
+		ease:Power3.easeIn
+	}); 
 	
-	TweenLite.to('.header-top', 3, {autoAlpha: 0, y:-46, ease:Power3.easeIn}); // in order. element in quotes, duration, and values in curly braces
-	// autoAlpha starts at 0 opacity and visibility of none
-	// power is the strength of the ease and can be between 1 and 4
-	// power1 easein starts slow and finishes fast and vise versa for easeout
+	/* 
+		in order. element in quotes, duration, and values in curly braces
+		autoAlpha starts at 0 opacity and visibility of none
+		power is the strength of the ease and can be between 1 and 4
+		power1 easein starts slow and finishes fast and vise versa for easeout
+	*/
+	
+	$('.service-slide').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true
+  	});
 	
 });
